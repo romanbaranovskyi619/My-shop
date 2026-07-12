b44835d5cb3ef438c098d9d110a21dc3fa09f287
romanbaranovskyi619-create <romanbaranovskyi619@gmail.com>
2026-07-12 13:02:27 +0300

    Create basic product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,6 +1,33 @@
+import { Link, useParams } from "react-router-dom";
+import products from "../data/products";
+import "./ProductPage.css";
+
 function ProductPage() {
+    const { id } = useParams();
+    const product = products.find(
+        (item) => item.id === Number(id)
+    );
     return (
-        <h1>Product Page</h1>
+        <div className="product-page">
+            <div className="product-image">
+                <img
+                    src={product.image}
+                    alt={product.name}
+                    width="400"
+                />
+            </div>
+
+            <div className="product-info">
+                <Link to="/">
+                    ← Back to Shop
+                </Link>
+                <h1>{product.name}</h1>
+
+                <h2>{product.price} $</h2>
+
+                <button>Add to Cart</button>
+            </div>
+        </div>
     );
 }
 
