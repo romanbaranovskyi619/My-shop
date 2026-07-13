a04cf5c8155dcfd7ed190087443c99d2c1e4c041
romanbaranovskyi619-create <romanbaranovskyi619@gmail.com>
2026-07-13 05:45:04 +0300

    feat: add not found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/product/:id" element={<ProductPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import "./NotFoundPage.css";
+
+function NotFoundPage() {
+    return (
+        <div className="not-found">
+            <h1>404</h1>
+            <p>Page not found</p>
+        </div>
+    );
+}
+
+export default NotFoundPage;
