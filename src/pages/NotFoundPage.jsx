04006323418a7308436ed6fcec059238f1cc539c
romanbaranovskyi619-create <romanbaranovskyi619@gmail.com>
2026-07-14 06:20:40 +0300

    fix: handle missing products gracefully

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,10 +1,19 @@
+import { Link } from "react-router-dom";
 import "./NotFoundPage.css";
 
-function NotFoundPage() {
+function NotFoundPage({
+    title = "404",
+    message = "Page not found",
+}) {
     return (
         <div className="not-found">
-            <h1>404</h1>
-            <p>Page not found</p>
+            <h1>{title}</h1>
+
+            <p>{message}</p>
+
+            <Link to="/">
+                Back to Shop
+            </Link>
         </div>
     );
 }
